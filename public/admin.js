84c03b1c9c87b899b6d21b8e86967aac31b3c3e8
madebygeelou <125709909+madebygeelou@users.noreply.github.com>
2024-02-08 22:02:58 -0800

    madechagnes

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,44 @@
 
 // Your Code Here
+async function main(){
+let books = await fetch('http://localhost:3001/listBooks'
+)
+let booksJSON = await books.json()
+console.log(booksJSON)
+
+booksJSON.forEach(function(book){
+    let div = document.createElement('div')
+    div.innerHTML = `
+    <img src= "${book.imageURL}" width ="200" />
+    <h3 id=${book.title}>${book.title}</h3>
+    <p>Year Published: ${book.year}</p>
+    <p>Quantity: ${book.quantity}</p>
+  <input id ="title-edit-${book.id}" type = "text"/>
+  <input type= "submit" onclick ="changeTitle(${book.id})"/>
+  
+    `
+    document.body.append(div)
+})
+}
+main()
+
+async function changeTitle(id){
+    let input = document.getElementById(index)
+    let value = input.value
+    
+    let response= await fetch('http://localhost:3001/updateBook')
+    method: 'PATCH',
+    headers; {
+        'Content-Type'; 'application/json'
+    }
+    body: JSON.stringify({
+        id: id,
+        title: value
+    })
+
+    let responseJSON = await response.json()
+console.log(responseJSON)
+}
+
+
+
